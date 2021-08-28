f525ddcc1adb1d3767329dda3a59d70ba4cfd6c9
akshay2211 <fxn769@gmail.com>
2021-08-28 18:08:37 +0000

    Update gh-pages to output generated at d43f6c2

---
+++ js/chunk-2d22d746.dc7da545.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var c=t("7a23"),a={class:"about"},s=Object(c["f"])("h1",null,"This is an about page",-1),u=[s];function o(n,e){return Object(c["q"])(),Object(c["e"])("div",a,u)}const i={};i.render=o;e["default"]=i}}]);
+//# sourceMappingURL=chunk-2d22d746.dc7da545.js.map
